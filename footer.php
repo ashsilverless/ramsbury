314bc29c0b5285736997fc746d423bffadd354ef
Ash - SIlverless <ash@silverless.co.uk>
2019-10-04 10:08:29 +0100

    DEV - GA code and cans loop

--- footer.php
+++ footer.php
@@ -10,70 +10,70 @@
 <footer class="footer">
 
     <div class="details">
-    
-        <?php get_template_part('template-parts/ramsbury', 'logo');?>        
-        
-        <p class="mt2"><?php the_field('footer_heading', 'option');?></p>      
-         
-        <p><?php the_field('shop_address', 'option');?></p>             
-        
-        <p><?php the_field('shop_telephone', 'option');?></p>          
-    
+
+        <?php get_template_part('template-parts/ramsbury', 'logo');?>
+
+        <p class="mt2"><?php the_field('footer_heading', 'option');?></p>
+
+        <p><?php the_field('shop_address', 'option');?></p>
+
+        <p><?php the_field('shop_telephone', 'option');?></p>
+
         <div class="socials mt1">
-        
+
             <?php if( have_rows('social_links', 'option') ): while( have_rows('social_links', 'option') ): the_row(); ?>
-            
+
             <a href="<?php the_sub_field('page_link'); ?>"><i class="fab fa-<?php the_sub_field('name'); ?>"></i></a>
-            
+
             <?php endwhile; endif; ?>
-        
+
         </div>
 
     </div>
 
     <div class="container-fluid pl0 pr0">
-	    
+
 	    <?php
 		    global $woocommerce;
-		    
+
 		    $myaccount_page_id = get_option( 'woocommerce_myaccount_page_id' );
-			
+
 			$account_url = $myaccount_page_id ? get_permalink( $myaccount_page_id ) : "";
-			
+
 			$cart_url = $woocommerce->cart->get_cart_url();
-		    
+
 		    $checkout_url = $woocommerce->cart->get_checkout_url();
-		    
+
 		?>
-    
+
         <div class="socket">
-    
+
             <div class="row">
-    
+
                 <div class="col-6">
-                    
-                    &copy; Ramsbury Brewery <?php echo date ('Y');?> | 
-                    
-                    <a href="/terms-conditions">Terms & Conditions</a> | 
-                    
+
+                    &copy; Ramsbury Brewery <?php echo date ('Y');?> |
+
+                    <a href="/terms-conditions">Terms & Conditions</a> |
+
                     <a href="/privacy">Privacy Policy</a>
-                        
+
                 </div>
-        
+
                 <div class="col-6 text-right">
-                    
-                    <a href="<?php echo $cart_url; ?>">Basket</a> | 
-                    
-                    <a href="<?php echo $checkout_url; ?>">Checkout</a> | 
-                                    
-                    <a href="<?php echo $account_url; ?>">My Account</a>                
-        
+
+                    <a href="<?php echo $cart_url; ?>">Basket</a> |
+
+                    <a href="<?php echo $checkout_url; ?>">Checkout</a> |
+
+                    <a href="<?php echo $account_url; ?>">My Account</a>
+
                 </div>
-        
+
             </div><!--row-->
-    
+
         </div><!--socket-->
-    
+
     </div><!--container-->
 
 </footer>
@@ -81,6 +81,18 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-149022531-1', 'auto');
+ga('send', 'pageview');
+</script>
+<!-- End Google Analytics -->
 
 </body>
 
--- template-parts/our-products.php
+++ template-parts/our-products.php
@@ -58,5 +58,32 @@
 
     </div><!--r-->
 
+    <div class="row">
+
+    <h2 class="heading  heading__sm heading__seperator mt1 mb2"><span><?php the_field('third_heading');?></span></h2>
+
+	<?php
+		$args = array(
+			'post_type' => 'product',
+			'product_cat' => 'cans-beer',
+			'posts_per_page' => -1
+			);
+		$loop = new WP_Query( $args );
+		if ( $loop->have_posts() ) {
+			while ( $loop->have_posts() ) : $loop->the_post(); ?>
+			
+			<div class="col-3 mb3 text-center">
+                <?php get_template_part('template-parts/product', 'card');
+?>
+            </div><?php
+
+			endwhile;
+		} else {
+			echo __( 'No products found' );
+		}
+		wp_reset_postdata();
+	?>
+
+    </div><!--r-->
 
 </div><!--c-->
